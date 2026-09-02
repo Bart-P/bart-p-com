9f013150d2c41742812cc3886f60740fb1bcf21e
bart-p <bartek.paraf@gmail.com>
2026-09-02 12:53:54 +0200

    feat: add technology model

--- contao/config/config.php
+++ contao/config/config.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Model\TechnologyModel;
+
 $GLOBALS['BE_MOD']['content']['technologies'] = [
     'tables' => ['tl_technology'],
 ];
+
+$GLOBALS['TL_MODELS']['tl_technology'] = TechnologyModel::class;
---
+++ src/Model/TechnologyModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model;
+
+use Contao\Model;
+
+class TechnologyModel extends Model
+{
+    protected static $strTable = 'tl_technology';
+}
